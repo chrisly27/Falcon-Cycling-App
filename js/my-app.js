dede94ac986db1b2aefbec2fc96bf43f00e99ba8
Crisly Domingos <bg70ng@student.sunderland.ac.uk>
2016-04-21 00:21:18 +0100

    created teh bike.json/main.css files and also I added a correct code for the category.html file as well as making sure the code works in my-app.js

--- js/my-app.js
+++ js/my-app.js
@@ -1,7 +1,7 @@
-/* //lets register template7 helper so we can pass json string in links
+//lets register template7 helper so we can pass json string in links
 Template.registerHelper('json_stringify', function(context){
 	return JSON.stringify(context);
-}); */
+});
 
 //initialise the appCodeName
 var myApp = new Framework7 ({
@@ -11,6 +11,16 @@ var myApp = new Framework7 ({
 	});
 
 var $$ = Dom7;
+
+//load the bike JSON file and add to the bikList dataset
+function getBikes(){
+		$$.getJSON('bike.json', function(json){
+			myApp.template7Data.bikeList = json;
+			console.log(template7Data);
+		});
+};
+
+getBikes();
 
 var mainView = myApp.addView('.view-main',{
 	//enable dynamic Navbar
