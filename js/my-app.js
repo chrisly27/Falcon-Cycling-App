1c51d68290028b38690c96b5c171c63cad16e16a
Crisly Domingos <bg70ng@student.sunderland.ac.uk>
2016-04-20 17:56:00 +0100

    modified index.html created about.html and also added code in my-app.js in order to show

--- js/my-app.js
+++ js/my-app.js
@@ -1,7 +1,7 @@
-//register template7 helper so we can pass json string in link
+/* //lets register template7 helper so we can pass json string in links
 Template.registerHelper('json_stringify', function(context){
 	return JSON.stringify(context);
-});
+}); */
 
 //initialise the appCodeName
 var myApp = new Framework7 ({
@@ -11,3 +11,8 @@ var myApp = new Framework7 ({
 	});
 
 var $$ = Dom7;
+
+var mainView = myApp.addView('.view-main',{
+	//enable dynamic Navbar
+	dynamicNavbar: true,
+});
